a4b4fe23a8e136db28ec97de84a37dcfa0cfe579
SuryaPran <suryagayathri2002@yahoo.com>
2022-01-02 18:21:58 -0500

    second commit in master

--- src/test/java/resources/TestDataBuild.java
+++ src/test/java/resources/TestDataBuild.java
@@ -22,9 +22,6 @@ public class TestDataBuild {
 		List<String> myList= new ArrayList();
 		myList.add("shoe park");
 		myList.add("shop");
-		myList.add("shop1");
-		myList.add("shop2");
-		myList.add("shop3");
 		p.setTypes(myList);
 		
 		Location l=new Location();
